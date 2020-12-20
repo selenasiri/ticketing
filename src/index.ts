3b3bd3660fdc24813dfa3b2641f0d3263c66405e
Selena Siri <selenasiri@gmail.com>
2020-12-20 14:39:32 -0500

    auth-depl

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
 
-
 const app = express();
 app.use(json());
 
+// Route handlers here
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
