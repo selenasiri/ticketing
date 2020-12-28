a653f6520f3784d464905474f4b0a35cc88969a4
Selena Siri <selenasiri@gmail.com>
2020-12-27 21:18:03 -0500

    tsconfig

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-// Route handlers here
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
